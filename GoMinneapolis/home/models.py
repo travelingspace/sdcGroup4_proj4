bc551d4f27e80e434594665e2f253dbf38dc6c5d
James Houck <jameshouck@live.com>
2019-10-21 09:14:34 -0500

    got an error trying to migrate model, needed to specify max length of char fields. Just fixed.

--- GoMinneapolis/home/models.py
+++ GoMinneapolis/home/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 
 # Create your models here.
-class FireReport(model.Model):
-    inci_no = models.CharField()
-    description = models.CharField()
+class FireReport(models.Model):
+    inci_no = models.CharField(max_length=100)
+    description = models.CharField(max_length=300)
     alm_date = models.DateField()
     alm_time = models.TimeField()
-    building_num = models.CharField()
-    street_num = models.CharField()
-    lattitude = models.DecimalField()
-    longitude = models.DecimalField()
-    incident_type = models.CharField()
+    building_num = models.CharField(max_length=100)
+    street_num = models.CharField(max_length=100)
+    lattitude = models.FloatField()
+    longitude = models.FloatField()
+    incident_type = models.CharField(max_length=100)
 
     def _str_(self):
         return f'{self.inci_no}, on {self.alm_date} at {self.alm_time} was reported at location address {self.building_num}, {self.street_num}. It was of incident type {self.incident_type}.'
