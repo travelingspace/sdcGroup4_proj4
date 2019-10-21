a443d11d45c9ffa2315d5dac55923628770d89c6
James Houck <jameshouck@live.com>
2019-10-21 09:09:35 -0500

    added FireReport to models.py

--- GoMinneapolis/home/models.py
+++ GoMinneapolis/home/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class FireReport(model.Model):
+    inci_no = models.CharField()
+    description = models.CharField()
+    alm_date = models.DateField()
+    alm_time = models.TimeField()
+    building_num = models.CharField()
+    street_num = models.CharField()
+    lattitude = models.DecimalField()
+    longitude = models.DecimalField()
+    incident_type = models.CharField()
+
+    def _str_(self):
+        return f'{self.inci_no}, on {self.alm_date} at {self.alm_time} was reported at location address {self.building_num}, {self.street_num}. It was of incident type {self.incident_type}.'
