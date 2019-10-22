895d709f74b4db96947f6c0a03b0f9cdbdd3cde9
James Houck <jameshouck@live.com>
2019-10-22 13:58:41 -0500

    added templates and hme view

--- GoMinneapolis/home/views.py
+++ GoMinneapolis/home/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import FireReport
 
 # Create your views here.
+
+def home(request):
+    incidents = FireReport.objects.all()
+    return render(request, 'home.html', { 'fire_incidents': incidents})
