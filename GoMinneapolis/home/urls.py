74fc1d31058f51d08f6b1b072f023e9eac94a727
James Houck <jameshouck@live.com>
2019-10-30 14:00:41 -0500

    trying to get URL paramter to work with a view

--- GoMinneapolis/home/urls.py
+++ GoMinneapolis/home/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.conf.urls import url
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('main/<int:zip_code>', views.main, name='main')
+    path('main/<int:zip_code>', views.main, name='main'),
+    path('getZip/$', views.getZipFromLatLong, name='getZip')
 ]
--- GoMinneapolis/home/views.py
+++ GoMinneapolis/home/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .models import LiquorSales
+from django.http import JsonResponse
 
 # Create your views here.
 
@@ -11,5 +12,13 @@ def home(request):
 def main(request, zip_code):
     ls = LiquorSales()
     lsales = ls.getBusinesses()
-    context = { 'liquorStos': lsales, 'zip_code': zip_code}
+    context = { 'liquorStos': lsales, 'zip_code': zip_code }
     return render(request, 'main.html', context)
+
+def getZipFromLatLong(request):
+    
+    latitude = request.GET.get('lat')
+    longitude = request.GET.get('lng')
+    ls = LiquorSales()
+    zipcode = ls.lat_long_to_zip(latitude, longitude)
+    return JsonResponse(zipcode)
