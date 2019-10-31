cbb5dbc6066befe0d5d46e89bc7bce0145025687
Nathan W <nathan@email.com>
2019-10-30 20:08:08 -0500

    unittest created

--- GoMinneapolis/home/tests.py
+++ GoMinneapolis/home/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from models import yelpRestaurants
 
 # Create your tests here.
+
+class DictLength(TestCase):
+
+    def test_length_of_list_made_by_yelp_api(self):
+        test_searchYelp = {}
+        yelpRestaurants.searchYelp(self, 55414)
+        test_list_length = len(test_searchYelp)
+        self.assertEquals(test_list_length, 20)
